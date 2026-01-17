38b57bd2d55ec79fb3d4138ca22d2a99cdab720d
Rosman Alvarez <rosmanalvarez52@gmail.com>
2026-01-17 16:22:03 -0500

    Incrementa version a 1.0.2 y versionCode a 3

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ export default ({ config }) => ({
 
     name: 'Taxi El Pangui',
     slug: 'taxi-pangui',
-    version: '1.0.1',
+    version: '1.0.2',
     orientation: 'portrait',
 
     icon: './assets/icon.png',
@@ -22,7 +22,7 @@ export default ({ config }) => ({
 
     android: {
       package: 'com.taxi.pangui',
-      versionCode: 2,
+      versionCode: 3,
       permissions: ['ACCESS_FINE_LOCATION', 'ACCESS_COARSE_LOCATION'],
       config: {
         googleMaps: {
